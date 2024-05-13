db68bfc1c32b3e34d3913011d7b7ed09362f3278
neoneoneo123 <gouxrunner@gmail.com>
2024-05-13 19:30:01 +0900

    add: mvvm pattern 적용을 위한 base code 작성

---
+++ app/src/main/java/com/example/dopaminemoa/mapper/VideoItemModel.kt
@@ -0,0 +1,9 @@
+package com.example.dopaminemoa.mapper
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class VideoItemModel(
+    val id: String
+) : Parcelable
