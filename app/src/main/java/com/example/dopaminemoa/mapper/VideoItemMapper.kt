08a71c657d519e895a6bf9ede5ba7b4ae413c1ff
neoneoneo123 <gouxrunner@gmail.com>
2024-05-14 15:10:07 +0900

    feat: api data class에 데이터 항목 추가 및 mapper에서 합칠 수 있도록 코드 작성

--- app/src/main/java/com/example/dopaminemoa/mapper/VideoItemMapper.kt
+++ app/src/main/java/com/example/dopaminemoa/mapper/VideoItemMapper.kt
@@ -2,9 +2,7 @@ package com.example.dopaminemoa.mapper
 
 import com.example.dopaminemoa.data.eachResponse.ChannelListResponse
 import com.example.dopaminemoa.data.eachResponse.SearchItems
-import com.example.dopaminemoa.data.eachResponse.SearchListResponse
 import com.example.dopaminemoa.data.eachResponse.VideoListResponse
-import com.example.dopaminemoa.repository.VideoRepository
 
 /**
  * api를 통해 응답 받은 데이터 중 view에서 사용할 데이터만 매핑하여 데이터 목록을 반환하는 함수입니다.
