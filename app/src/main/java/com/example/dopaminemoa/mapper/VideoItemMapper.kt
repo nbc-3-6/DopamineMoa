4fef74f5e3ba5efc28e30884b3fc1f79c7bfe5ee
neoneoneo123 <gouxrunner@gmail.com>
2024-05-14 14:10:35 +0900

    feat: mapper에 추가할 데이터 준비

--- app/src/main/java/com/example/dopaminemoa/mapper/VideoItemMapper.kt
+++ app/src/main/java/com/example/dopaminemoa/mapper/VideoItemMapper.kt
@@ -1,13 +1,52 @@
 package com.example.dopaminemoa.mapper
 
+import com.example.dopaminemoa.data.eachResponse.ChannelListResponse
+import com.example.dopaminemoa.data.eachResponse.SearchItems
+import com.example.dopaminemoa.data.eachResponse.SearchListResponse
+import com.example.dopaminemoa.data.eachResponse.VideoListResponse
+import com.example.dopaminemoa.repository.VideoRepository
+
 /**
- * 작성자 : 정지연
  * api를 통해 응답 받은 데이터 중 view에서 사용할 데이터만 매핑하여 데이터 목록을 반환하는 함수입니다.
  */
-//fun List<>.asVideoItemModel(): List<VideoItemModel> {
-//    return map {
-//        VideoItemModel(
-//            it.
-//        )
+
+object VideoItemMapper {
+    fun fromSearchItems(searchItems: List<SearchItems>): List<VideoItemModel> {
+        return searchItems.map {
+            VideoItemModel(
+                videoId = it.id.videoId,
+                videoTitle = it.snippet.title,
+                videoThumbnail = it.snippet.thumbnails.toString(),
+                videoViews = "",
+                videoDescription = it.snippet.description,
+                channelTitle = it.snippet.channelTitle,
+                channelThumbnails = ""
+            )
+        }
+    }
+
+//    fun updateVideoViews(videoItems: List<VideoItemModel>, videoListResponse: VideoListResponse) : List<VideoItemModel> {
+//        return videoItems.map { videoItem ->
+//            val video = videoListResponse.items.find { it.id == videoItem.videoId }
+//            videoItem.copy(videoViews = video?.statictics?.viewCount ?: "")
+//        }
 //    }
-//}
+//
+//    fun updateChannelThumbnails(videoItems: List<VideoItemModel>, channelListResponse: ChannelListResponse) : List<VideoItemModel> {
+//        return videoItems.map { videoItem ->
+//            val video = ChannelListResponse.items.find { it.id == videoItem.videoId }
+//            videoItem.copy(channelThumbnails = channel?.snippet?.thumbnails.toString() ?: "")
+//        }
+//    }
+}
+
+
+
+/**
+ * 비디오 제목 - search
+ * 비디오 썸네일 - search
+ * 비디오 조회수 - video
+ * 비디오 설명 - search
+ * 채널 이름 - search
+ * 채널 썸네일 - channel
+ */
--- app/src/main/java/com/example/dopaminemoa/mapper/VideoItemModel.kt
+++ app/src/main/java/com/example/dopaminemoa/mapper/VideoItemModel.kt
@@ -1,9 +1,17 @@
 package com.example.dopaminemoa.mapper
 
 import android.os.Parcelable
+import com.example.dopaminemoa.data.eachResponse.SearchThumbnails
+import com.example.dopaminemoa.data.eachResponse.VideoThumbnail
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class VideoItemModel(
-    val id: String
+    val videoId: String,
+    val videoTitle: String,
+    val videoThumbnail: String,
+    val videoViews: String,
+    val videoDescription: String,
+    val channelTitle: String,
+    val channelThumbnails: String
 ) : Parcelable
