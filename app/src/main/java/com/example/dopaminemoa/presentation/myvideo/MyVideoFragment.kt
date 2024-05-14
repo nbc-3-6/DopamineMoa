393cf9688460ba5c3335a15e0eb0169b21b76912
kang <guswjd9994@duksung.ac.kr>
2024-05-14 15:56:17 +0900

    rename: 프래그먼트 명 수정

--- app/src/main/java/com/example/dopaminemoa/presentation/myvideo/MyVideoFragment.kt
+++ app/src/main/java/com/example/dopaminemoa/presentation/myvideo/MyVideoFragment.kt
@@ -1,4 +1,4 @@
-package com.example.dopaminemoa.presentation.myvideos
+package com.example.dopaminemoa.presentation.myvideo
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.dopaminemoa.R
 
-class MyVideosFragment : Fragment() {
+class MyVideoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my_videos, container, false)
+        return inflater.inflate(R.layout.fragment_my_video, container, false)
     }
 
 }
