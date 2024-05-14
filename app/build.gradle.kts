4124828b37f9dd71112cde24da97c57ed676a008
Bettor <be0128st@gmail.com>
2024-05-15 02:17:34 +0900

    add: glide implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
 
 
     // viewmodel
