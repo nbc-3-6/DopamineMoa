b2c744b938b3f406d5490af4a943ccd2f8e04c9b
Combro-Kim <rlawog@nate.com>
2024-05-14 12:38:02 +0900

    MainFragment,item xml 작업

--- app/src/main/java/com/example/dopaminemoa/Home/HomeFragment.kt
+++ app/src/main/java/com/example/dopaminemoa/Home/HomeFragment.kt
@@ -1,15 +1,21 @@
-package com.example.dopaminemoa
+package com.example.dopaminemoa.Home
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.dopaminemoa.databinding.FragmentHomeBinding
+import com.example.dopaminemoa.viewmodel.VideoViewModel
+import com.example.dopaminemoa.viewmodel.VideoViewModelFactory
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private val viewmodel: VideoViewModel by viewModels({ requireActivity() }) {
+        VideoViewModelFactory.newInstance()
+    }
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
