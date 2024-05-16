78ba8537781a78182112dafcfe0269a5e3257bb9
theBettor <be0128st@gmail.com>
2024-05-16 16:46:19 +0900

    rename: 함수명 변경

--- app/src/main/java/com/example/dopaminemoa/presentation/myvideo/MyVideoViewModel.kt
+++ app/src/main/java/com/example/dopaminemoa/presentation/myvideo/MyVideoViewModel.kt
@@ -9,7 +9,7 @@ import com.example.dopaminemoa.data.remote.RemoteDataSource
 import com.example.dopaminemoa.mapper.VideoItemModel
 import com.example.dopaminemoa.utils.Utils
 
-class MyVideoViewModel(private val apiService: RemoteDataSource) : ViewModel() {
+class MyVideoViewModel : ViewModel() {
 
     // 좋아요 아이템들에 대한 MutableLiveData 선언
     private val _likedItems = MutableLiveData<List<VideoItemModel>>()
@@ -18,15 +18,15 @@ class MyVideoViewModel(private val apiService: RemoteDataSource) : ViewModel() {
     val likedItems: LiveData<List<VideoItemModel>> get() = _likedItems
 
     // 저장된 좋아요 아이템들을 가져오는 함수
-    fun getBookmarkedItems(context: Context) {
+    fun getLikedItems(context: Context) {
         // Utils 클래스를 이용해 저장된 좋아요를 가져와서 _likedItems에 저장
-        _likedItems.value = Utils.getPrefBookmarkItems(context)
+        _likedItems.value = Utils.getPrefLikeItems(context)
     }
     // 특정 아이템을 삭제하는 함수
     fun deleteItem(context: Context, item: VideoItemModel, position: Int) {
         // Utils 클래스를 이용해 아이템 삭제
-        Utils.deletePrefItem(context, item.id)
-        Log.d("VideoItemModel", "dopaminemoa deleteItem position=${position}, id = ${item.id}")
+        Utils.deletePrefItem(context, item.videoId)
+        Log.d("VideoItemModel", "dopaminemoa deleteItem position=${position}, id = ${item.videoId}")
 
         // 삭제된 아이템 정보를 반영하여 LiveData 업데이트
         _likedItems.value?.let { currentItems ->
