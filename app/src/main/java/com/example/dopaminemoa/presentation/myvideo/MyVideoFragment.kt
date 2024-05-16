07616341841cb3f920f2a583b6d197389ccf7750
theBettor <be0128st@gmail.com>
2024-05-16 16:45:49 +0900

    feat: fragment mvvm 마무리

--- app/src/main/java/com/example/dopaminemoa/presentation/myvideo/MyVideoFragment.kt
+++ app/src/main/java/com/example/dopaminemoa/presentation/myvideo/MyVideoFragment.kt
@@ -7,20 +7,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.dopaminemoa.databinding.FragmentMyVideoBinding
+import com.example.dopaminemoa.mapper.VideoItemModel
 
 class MyVideoFragment : Fragment() {
 
-    private lateinit var binding: FragmentMyVideoBinding
+    private var binding: FragmentMyVideoBinding? = null
     private lateinit var adapter: MyVideoAdapter
     private lateinit var gridmanager: StaggeredGridLayoutManager
     private lateinit var mContext: Context
+
+//    val sharedViewModel by activityViewModels<SharedViewModel>()
+    private val viewModel: MyVideoViewModel by viewModels()
 
     companion object {
         fun newInstance() = MyVideoFragment()
     }
 
+    // 프래그먼트가 액티비티에 붙을 때 호출
     override fun onAttach(context: Context) {
         super.onAttach(context)
         mContext = context
@@ -29,27 +36,48 @@ class MyVideoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentMyVideoBinding.inflate(inflater, container, false)
-        return binding.root
-        setupViews()
+    ): View? {
+        // 어댑터 초기화
+        adapter = MyVideoAdapter(mContext)
+
+        // 바인딩 설정
+        binding = FragmentMyVideoBinding.inflate(inflater, container, false).apply {
+            rvMyVideo.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            rvMyVideo.adapter = adapter
+            rvMyVideo.visibility = View.GONE
+        }
+
+        // 좋아요된 아이템 로딩
+        viewModel.getLikedItems(mContext)
+
+        // 좋아요 리스트 관찰하여 UI 업데이트
+        viewModel.likedItems.observe(viewLifecycleOwner) { likes ->
+            adapter.items = likes.toMutableList() as ArrayList<VideoItemModel>
+            adapter.notifyDataSetChanged()
+        }
+
+        // 항목 클릭 시 동작 정의
+        adapter.setOnItemClickListener(object : MyVideoAdapter.OnItemClickListener {
+            override fun onItemClick(item: VideoItemModel, position: Int) {
+                viewModel.deleteItem(mContext, item, position)
+                Log.d("MyVideoFragment", "#dopamine onItemClick deleteItem position = $position")
+//                sharedViewModel.addDeletedItemUrls(item.id)
+
+            }
+        })
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btnBack.setOnClickListener {
+        binding?.btnBack?.setOnClickListener {
             requireActivity().supportFragmentManager.popBackStack()
-            Log.d("되냐?", binding.btnBack.toString())
+            Log.d("되냐?", binding?.btnBack.toString())
         }
     }
-
-    private fun setupViews() {
-        // RecyclerView 설정
-        gridmanager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-        binding.rvMyVideo.layoutManager = gridmanager
-
-        adapter = MyVideoAdapter(mContext)
-        binding.rvMyVideo.adapter = adapter
-        binding.rvMyVideo.itemAnimator = null
+    // 프래그먼트 뷰 종료 시 호출
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null  // 바인딩 리소스 해제
     }
 }
