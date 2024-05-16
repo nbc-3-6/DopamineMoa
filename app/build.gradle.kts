c638d96b6fe403e7ef40dae1c6bebf81eb631a11
neoneoneo123 <gouxrunner@gmail.com>
2024-05-16 11:59:30 +0900

    feat: okhttp 통신 log 볼 수 있도록 작성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     // Networking with Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
 
     //circle image view
