cb4f47181814bfaa4f373fd3173ee04a22b2ef8b
kang <guswjd9994@duksung.ac.kr>
2024-05-16 16:50:44 +0900

    refactor: companion object로 변경

--- app/src/main/java/com/example/dopaminemoa/Const.kt
+++ app/src/main/java/com/example/dopaminemoa/Const.kt
@@ -1,3 +1,12 @@
 package com.example.dopaminemoa
 
-const val API_KEY = "AIzaSyAjgPka06eoQYfJTcqLEtlgcWn1-6nMIyw"
+class Const {
+    companion object { // 상수들은 어디서든지 접근 가능하고 변경되지 않아야 하므로, 정적 멤버로 선언
+        // retrofit
+        const val API_KEY = "AIzaSyAjgPka06eoQYfJTcqLEtlgcWn1-6nMIyw"
+
+        // sharedPreference
+        const val PREFERENCE_NAME = "pref"
+        const val Liked_ITEMS = "liked_items"
+    }
+}
