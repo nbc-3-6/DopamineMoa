c99d31ae3603a7015f623fca29b424ed75befa20
Bettor <be0128st@gmail.com>
2024-05-21 16:51:54 +0900

    refactor: ViewModel, getStorageItems만 사용, Factory

--- app/src/main/java/com/example/dopaminemoa/presentation/myvideo/MyVideoViewModel.kt
+++ app/src/main/java/com/example/dopaminemoa/presentation/myvideo/MyVideoViewModel.kt
@@ -11,59 +11,31 @@ import com.example.dopaminemoa.repository.VideoRepository
 import kotlinx.coroutines.launch
 
 class MyVideoViewModel(
-    private val videoRepository: VideoRepository,
-    context: Context
+    private val videoRepository: VideoRepository
 ) : ViewModel() {
 
-
     // 좋아요 아이템들에 대한 MutableLiveData 선언
-    private val _likedItems = MutableLiveData<VideoItemModel>()
+    private val _likedItems: MutableLiveData<List<VideoItemModel>> = MutableLiveData()
 
     // 외부에서 관찰할 수 있도록 LiveData로 제공
-    val likedItems: LiveData<VideoItemModel> get() = _likedItems
+    val likedItems: LiveData<List<VideoItemModel>> get() = _likedItems
 
     // 저장된 좋아요 아이템들을 가져오는 함수
-    private fun getLikedItems(videoItemModel: VideoItemModel) {
+    fun getLikedItems() {
         viewModelScope.launch {
-            videoRepository.saveVideoItem(videoItemModel.copy(isLiked = true))
+            _likedItems.value = videoRepository.getStorageItems()
         }
     }
 
-    private fun deleteVideoItem(videoItemModel: VideoItemModel) {
-        viewModelScope.launch {
-            videoRepository.removeVideoItem(videoItemModel.copy(isLiked = false))
-        }
-    }
-
-    fun updatelikedItem(videoItemModel: VideoItemModel) {
-        viewModelScope.launch {
-            if (videoItemModel.isLiked) {
-                getLikedItems(videoItemModel)
-            } else {
-                deleteVideoItem(videoItemModel)
+    class MyVideoViewModelFactory(
+        private val videoRepository: VideoRepository
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(MyVideoViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return MyVideoViewModel(videoRepository) as T
             }
+            throw IllegalArgumentException("Unknown ViewModel class")
         }
-    }
-
-    fun isVideoLikedInPrefs(videoId: String?): Boolean {
-        return videoRepository.isVideoLikedInPrefs(videoId)
-    }
-
-    fun clearPreferences() {
-        videoRepository.clearPrefs()
-    }
-
-}
-
-class MyVideoViewModelFactory(
-    private val videoRepository: VideoRepository,
-    private val context: Context
-) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(MyVideoViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return MyVideoViewModel(videoRepository, context) as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
