90610eb89a52f9bc048cfd3686ecc02e298ac9bf
Bettor <be0128st@gmail.com>
2024-05-22 18:26:09 +0900

    MyVideoFragment (#18)
    * layout: MyVideoFragment
    
    pull 받기 전
    
    * add: angle_right.xml, strings.xml
    
    로컬에 추가 안되어있어서 추가
    
    * fix: strings.xml, string name 두 개 생겨서 하나 삭제
    
    * fix: pull 받으면서 생긴 conflict 해결
    
    * rename: MyVideoFragment가 아니라 myvideos로 되어있어서 변경
    
    * rename: 바른 이름의 Fragment로 변경 하고, 바른 위치의 package로 이동 하였습니다.
    
    다른 분이 초기에 임시로 만들어주신 담당 프래그먼트도 지웠습니다
    
    * add: Utils
    
    * add: VideoViewModel
    
    * layout: iv_profie, tv_title, tv_description 변경
    
    * add: Constants
    
    * add: glide implementation
    
    * refactor: VideoItemModel
    
    프로퍼티 임의 추가
    
    * refactor: Adapter
    
    정답은 아니고, shared 고려하지 않고 수정함
    
    * rename: 주신 Model 적용하기
    
    * refactor: 어댑터에 sharedpreference
    
    * feat: fragment mvvm 마무리
    
    * rename: 함수명 변경
    
    * rename: Utils 프로퍼티명 및 함수명 변경
    
    * 에러해결한 브랜치
    
    * refactor: 뷰모델과 매퍼에 사용할 프로퍼티 추가
    
    * refactor: 모델 수정
    
    * refactor: 더미데이터와 쓸데없는 주석 흔적 지우기
    
    * refactor: 재도전
    
    * 삽질 . 그어딘가
    
    * refactor : itemclick 이벤트 처리 추가
    
    * refactor: adapter, factory, livedata, event처리
    
    더 수정 필요
    
    * refactor: ViewModel, getStorageItems만 사용, Factory
    
    * rename: item 설정 변경 및 행 삭제
    
    * refactor: fragment layout margintop 삭제
    
    * refactor: Update를 감지하는 메소드 추가
    
    * refactor: 좋아요 버튼 간헐적 불통 해결
    
    공유에는 clickable이 없길래, ll_like에 있는 것을 삭제했습니다.
    
    * refactor: 아이템 클릭, 코드 정리
    
    * refactor: 아이템 역순으로 쌓기, 지연님 SearchItem xml로 대체,
    
    * refactor: 백버튼 지우기
    
    * refactor: Detail에서 백버튼 2번 눌러야 정상 작동되는 현상
    
    * feat: 스크롤 위치 초기화
    
    * refactor:  안쓰는 코드 삭제
    
    * feat: 아이템이 바로 갱신이 안되는데...
    
    * remove: 안쓰는 코드 삭제
    
    * fix: 데이터 변경이 리사이클러뷰에 실시간으로 반영이 되지 않는 문제
    
    setFragmentResultListener로 손쉽게 해결
    
    * refactor: rv 피그마 참고해서 수정
    
    지연님의 searchitem 갖다 쓰면서 피그마랑 다른점이 생겨서 marginbottom 12dp 추가

---
+++ app/src/main/java/com/example/dopaminemoa/presentation/myvideo/MyVideoAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.dopaminemoa.presentation.myvideo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.dopaminemoa.databinding.SearchItemBinding
+import com.example.dopaminemoa.mapper.model.VideoItemModel
+
+class MyVideoAdapter : RecyclerView.Adapter<MyVideoAdapter.MyVideoViewHolder>() {
+
+    var itemClick: ItemClick? = null
+    private val items: MutableList<VideoItemModel> = mutableListOf()
+
+    fun updateList(newItems: List<VideoItemModel>) {
+        items.clear()
+        items.addAll(newItems.reversed())
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyVideoViewHolder {
+        val binding = SearchItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return MyVideoViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: MyVideoViewHolder, position: Int) {
+        holder.bind(items[position])
+        holder.itemView.setOnClickListener {
+            itemClick?.onClick(it, items[position])
+            notifyDataSetChanged()
+        }
+    }
+
+    class MyVideoViewHolder(private val binding: SearchItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: VideoItemModel) = with(binding) {
+            tvTitle.text = item.videoTitle
+            tvChannel.text = item.channelTitle
+            Glide.with(itemView).load(item.videoThumbnail).into(ivItem)
+        }
+    }
+
+    interface ItemClick {
+        fun onClick(view: View, item: VideoItemModel)
+    }
+}
--- app/src/main/java/com/example/dopaminemoa/presentation/myvideo/MyVideoFragment.kt
+++ app/src/main/java/com/example/dopaminemoa/presentation/myvideo/MyVideoFragment.kt
@@ -1,23 +1,122 @@
 package com.example.dopaminemoa.presentation.myvideo
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.dopaminemoa.R
-import com.example.dopaminemoa.presentation.home.HomeFragment
+import androidx.fragment.app.setFragmentResultListener
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.dopaminemoa.databinding.FragmentMyVideoBinding
+import com.example.dopaminemoa.mapper.model.VideoItemModel
+import com.example.dopaminemoa.network.RepositoryClient
+import com.example.dopaminemoa.presentation.main.MainActivity
+import com.example.dopaminemoa.presentation.videodetail.VideoDetailFragment
+import com.example.dopaminemoa.presentation.videodetail.VideoDetailFragment.Companion.BUNDLE_KEY_FOR_DETAIL_FRAGMENT
+import com.example.dopaminemoa.repository.VideoRepositoryImpl
 
 class MyVideoFragment : Fragment() {
-    companion object {
-        fun newInstance() = MyVideoFragment()
+
+    private var _binding: FragmentMyVideoBinding? = null
+    private val binding get() = _binding!!
+
+    private var adapter = MyVideoAdapter()
+
+    private val viewModel: MyVideoViewModel by viewModels {
+        MyVideoViewModel.MyVideoViewModelFactory(
+            VideoRepositoryImpl(RepositoryClient.youtubeService, requireContext())
+        )
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_my_video, container, false)
+    ): View {
+        _binding = FragmentMyVideoBinding.inflate(inflater, container, false)
+        binding.rvMyVideo.layoutManager =
+            GridLayoutManager(requireActivity(), 2, GridLayoutManager.VERTICAL, false)
+        binding.rvMyVideo.adapter = adapter
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setFragmentResult()
+        arguments?.getParcelable<VideoItemModel>(BUNDLE_KEY_FOR_MYVIDEO_FRAGMENT)
+        setUpView()
+        itemClick()
+    }
+
+    fun setUpView() {
+        viewModel.likedItems.observe(viewLifecycleOwner) { items ->
+            val items = viewModel.likedItems.value
+            if (items != null) {
+                adapter.updateList(items) // 1. 변화가 있을때 변경해줄 준비
+            }
+        }
+        // 1, 2번은 거의 동시에 실행됨
+        viewModel.getLikedItems() // 2. livedata 준비
+    }
+
+    fun itemClick() {
+        // 4번도 있었는데 2번 코드랑 같고, 아이템 클릭처리해서 디테일가는건데 뭐하러 view의 변화를 update로 해결해야하나 싶어서
+        // 주석해봤더니 괜찮아서 지웠음
+        viewModel.likedItems.observe(viewLifecycleOwner) {
+            adapter.itemClick = object : MyVideoAdapter.ItemClick {
+                override fun onClick(view: View, item: VideoItemModel) {
+                    item.let {
+                        clickItem(item) // 3. 클릭했을 때, 디테일로 가기
+                        viewModel.updateSaveItem(it)  // 4. ViewModel에서 업데이트
+                    }
+                }
+            }
+        }
+    }
+
+    private var isItemClickEnabled = true
+
+    private fun clickItem(item: VideoItemModel) {
+        if (!isItemClickEnabled) return
+        isItemClickEnabled = false
+
+        val bundle = Bundle().apply {
+            putParcelable(BUNDLE_KEY_FOR_DETAIL_FRAGMENT, item)
+        }
+        val detailFragment = VideoDetailFragment.newInstance(bundle)
+        (requireActivity() as MainActivity).showVideoDetailFragment(detailFragment)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            isItemClickEnabled = true
+        }, 500) // 500ms 딜레이
+    }
+
+    fun setFragmentResult() {
+        // FragmentResultListener 추가
+        setFragmentResultListener("video_detail_result") { requestKey, bundle ->
+            val isLikedChanged = bundle.getBoolean("is_liked_changed")
+            if (isLikedChanged) {
+                viewModel.getLikedItems()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.rvMyVideo.scrollToPosition(0) // 스크롤 위치를 초기화
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null  // 바인딩 리소스 해제
+    }
+
+    companion object {
+        const val BUNDLE_KEY_FOR_MYVIDEO_FRAGMENT = "BUNDLE_KEY_FOR_MYVIDEO_FRAGMENT"
+        fun newInstance() = MyVideoFragment()
+    }
 }
